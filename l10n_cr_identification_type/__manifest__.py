08aa0da410ad61817dd51d5a2cb42d4191d596e0
Hector Manuel Aular <aular.hector.dev@gmail.com>
2023-02-18 19:18:47 -0300

    [ADD] Models l10n_cr_identification_type

---
+++ l10n_cr_identification_type/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'Type document localitation CR',
+    'version': '14.0.0.1',
+    'summary': '',
+    'description': '',
+    'category': 'Localitation',
+    'author': 'aular.hector.dev@gmail.com',
+    'license': '',
+    'depends': [
+        'l10n_latam_invoice_document'
+    ],
+    'data': [
+        'data/identification_type_data.xml',
+        'views/res_partner.xml'
+    ],
+}
